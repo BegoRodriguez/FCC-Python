6a75c19323f96566d4c24a751f659ac9fc0426b9
BegoRodriguez <136585766+BegoRodriguez@users.noreply.github.com>
2024-06-22 10:18:04 +0200

    Comprobaciones realizadas, paso a formatear

--- 06-Arithmetic-formatter.py
+++ 06-Arithmetic-formatter.py
@@ -12,38 +12,49 @@ def arithmetic_arranger(problems, show_answers=False):
     if len(problems)>5:
         return 'Error: Too many problems.'
 
-    """The appropriate operators the function will accept are addition and 
-    subtraction. Multiplication and division will return an error. 
-    Other operators not mentioned in this bullet point will not 
-    need to be tested. The error returned will be: "Error: Operator 
-    must be '+' or '-'."""
     # Para llegar a hacer esto debería descomponer cada elemento del array
     operacion = []
     for problem in problems:
         print(problem)
           ## Igual podría usar finds en vez de un bucle
         primer_espacio = problem.find(" ")
-        primer_operando = problem[0:problem.find(" ")]        
-        # Tengo que añadir 1 para que busque el siguiente
-        segundo_espacio = problem.find(" ",primer_espacio+1) 
-        operador = problem[primer_espacio+1:segundo_espacio] 
-
+        primer_operando = problem[0:primer_espacio]        
+        # Tengo que añadir 1 para que busque el siguiente espacio
+        segundo_espacio = problem.find(" ",primer_espacio+1)
+        operador = problem[primer_espacio+1:segundo_espacio]
+        segundo_operando = problem[segundo_espacio+1:]
+        
+        """The appropriate operators the function will accept are addition and 
+        subtraction. Multiplication and division will return an error. 
+        Other operators not mentioned in this bullet point will not 
+        need to be tested. The error returned will be: "Error: Operator 
+        must be '+' or '-'."""
         # Aquí ya puedo hacer la comprobación de si el operador es '+' or '-' 
         if operador != '+' and operador != '-':
             return "Error: Operator must be '+' or '-'."
 
-        segundo_operando = problem[segundo_espacio+1:]
+        """Each number (operand) should only contain digits. Otherwise, 
+        the function will return: 'Error: Numbers must only contain digits.'"""
+        # ¿Cómo compruebo esto?
+        if not(primer_operando.isdigit()) or not(segundo_operando.isdigit()):
+            return "Error: Numbers must only contain digits."
+        
+        """Each operand (aka number on each side of the operator) has a max of 
+        four digits in width. Otherwise, the error string returned will be: 
+        'Error: Numbers cannot be more than four digits.'"""
+        # Puedo hacer esta comprobación con los operandos
+        if len(primer_operando)>4 or len(segundo_operando)>4 :
+           return "Error: Numbers cannot be more than four digits."
         
         ## Guardo en un diccionario y lo añado al array
         # expenses.append({'amount': amount, 'category': category})
         operacion.append({'primer_operando':primer_operando,'operador':operador,'segundo_operando':segundo_operando})
 
     print(operacion)
-
-
-
+    
+    # Comprobaciones realizadas, paso a formatear
 
     return problems
 
 print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
-print(f'\n{arithmetic_arranger(["32 * 698", "3801 / 2", "45 + 43", "123 + 49"])}')
+print(f'\n{arithmetic_arranger(["32 + 69888", "3801 / 2", "45 + 43", "123 + 49"])}')
